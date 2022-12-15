c1671ced1fe349e4370eed7baa38da0df8491c9b
k-omotani <k.omotani.0225@gmail.com>
2022-12-15 16:49:48 +0900

    list containers

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/manifoldco/promptui"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
-}
 
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	cli.NegotiateAPIVersion(ctx)
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	if len(containers) == 0 {
+		fmt.Print("no containers")
+		return
+	}
+
+	var containerNames []string
+	for _, container := range containers {
+		containerNames = append(containerNames, container.Image)
+	}
+
+	prompt := promptui.Select{
+		Label: "Select image",
+		Items: containerNames,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+}
